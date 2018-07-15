5f141ecfb5dd34c35c31ff74667ff52c7f2cf4bc
FrankieMarie <frankiemarie83@gmail.com>
2018-07-15 08:19:27 -0700

    customer by id page styled

--- client/src/components/CustomerById.js
+++ client/src/components/CustomerById.js
@@ -12,27 +12,49 @@ class CustomerById extends Component {
   render() {
     let current;
     if(this.props.customer.customer){
-      const {name, email, phone, workTradeHours, date} = this.props.customer.customer
+      const {name, email, phone, workTradeHours, date, _id } = this.props.customer.customer
       return (
         current = (
           <div className="customer-by-id">
+
             <h2 className="name-by-id">{name}</h2>
-              <p className="customer-info">
-                <small className="small">Email:</small>
-                <span>{email}</span>
-              </p>
-              <p className="customer-info">
-                <small className="small">Phone:</small>
-                <span>{phone}</span>
-              </p>
-              <p className="customer-info">
-                <small className="small">Work Trade Hours:</small>
-                <span>{workTradeHours}</span>
-              </p>
-              <p className="customer-info">
-                <small className="small">Date Added:</small>
-                <span>{moment(date).format('MMMM D, YYYY')}</span>
-              </p>
+
+            <ul className="customer-ul">
+
+              <li className="customer-item">
+                <p className="customer-info">
+                  <small className="small">Email: </small>
+                  <span className="customer-content">{email}</span>
+                </p>
+              </li>
+
+              <li className="customer-item">
+                <p className="customer-info">
+                  <small className="small">Phone: </small>
+                  <span className="customer-content">{phone}</span>
+                </p>
+              </li>
+
+              <li className="customer-item">
+                <p className="customer-info">
+                  <small className="small">Work Trade: </small>
+                  <span className="customer-content">{workTradeHours} hours</span>
+                </p>
+              </li>
+
+              <li className="customer-item">
+                <p className="customer-info">
+                  <small className="small">Date Added: </small>
+                  <span className="customer-content">{moment(date).format('MMMM D, YYYY')}</span>
+                </p>
+              </li>
+
+            </ul>
+
+            <a href={`/edit/${_id}`} className="customer-edit">
+              <i className="fas fa-user-edit customer-edit-icon"></i> Edit
+            </a>
+
           </div>
         )
       )

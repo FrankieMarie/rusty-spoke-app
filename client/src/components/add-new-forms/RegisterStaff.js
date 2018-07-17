3c347fe7f9114576d795de718044fbecbbc143cc
FrankieMarie <frankiemarie83@gmail.com>
2018-07-17 06:27:33 -0700

    register staff member form working

--- client/src/components/add-new-forms/RegisterStaff.js
+++ client/src/components/add-new-forms/RegisterStaff.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { registerStaff } from '../../redux/actions/staffActions'
+import { addStaff } from '../../redux/actions/staffActions'
 
 class RegisterStaff extends Component {
   state = {
@@ -10,39 +10,62 @@ class RegisterStaff extends Component {
       phone: ''
   }
 
-  onSubmit(e) {
-    e.preventDefault();
-    const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      phone: this.state.phone
-    }
-    this.props.registerStaff(newUser)
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addStaff(this.state, this.props.history)
   }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input
-            type="text"
-            placeholder="name"
-            value={this.state.name}
-            onChange={e => this.setState({name: e.target.value})}
-          />
-          <input
-            type="text"
-            placeholder="email"
-            value={this.state.email}
-            onChange={e => this.setState({email: e.target.value})}
-          />
-          <input
-            type="text"
-            placeholder="phone"
-            value={this.state.phone}
-            onChange={e => this.setState({phone: e.target.value})}
-          />
-          <button>Submit</button>
+      <div className="edit-form">
+        <h1 className="purchases-h1">New Staff</h1>
+        <form onSubmit={this.handleSubmit.bind(this)} className="edit-inputs">
+
+          <div className="input-group">
+            <label
+              htmlFor="name"
+              style={{color: "#18d369", letterSpacing: ".05rem"}}>
+              Full Name
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={e => this.setState({name: e.target.value})}
+            />
+          </div>
+
+          <div className="input-group">
+            <label
+              htmlFor="email"
+              style={{color: "#18d369", letterSpacing: ".05rem"}}>
+              Email
+            </label>
+            <input
+              className="form-input"
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={e => this.setState({email: e.target.value})}
+            />
+          </div>
+
+          <div className="input-group">
+            <label
+              htmlFor="phone"
+              style={{color: "#18d369", letterSpacing: ".05rem"}}>
+              Phone
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={e => this.setState({phone: e.target.value})}
+            />
+          </div>
+          <button type="submit" className="update-shift-btn">Submit</button>
         </form>
       </div>
     )
@@ -50,7 +73,7 @@ class RegisterStaff extends Component {
 }
 
 RegisterStaff.propTypes = {
-  registerStaff: PropTypes.func.isRequired
+  addStaff: PropTypes.func.isRequired
 }
 
-export default connect(null, { registerStaff })(RegisterStaff)
+export default connect(null, { addStaff })(RegisterStaff)
